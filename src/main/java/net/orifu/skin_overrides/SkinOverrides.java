7ee0b597f0229495c9d941b1cd7fe4e5460400d0
orifu <orifu@duck.com>
2024-06-15 23:26:10 +0100

    ctrl x ctrl v

--- src/main/java/net/orifu/skin_overrides/SkinOverrides.java
+++ src/main/java/net/orifu/skin_overrides/SkinOverrides.java
@@ -5,14 +5,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.mojang.authlib.GameProfile;
+import com.mojang.util.UndashedUuid;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.texture.PlayerSkin;
+import net.minecraft.util.Identifier;
 import net.orifu.skin_overrides.texture.LocalHttpTexture;
 import net.orifu.skin_overrides.texture.LocalSkinTexture;
 
@@ -23,6 +27,70 @@ public class SkinOverrides {
 	public static final String CAPE_OVERRIDES = "cape_overrides";
 
 	public static final String UUID_REGEX = "^[0-9a-f]{8}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{12}$";
+
+	public static PlayerSkin getSkin(GameProfile profile) {
+		return overrideSkin(profile, MinecraftClient.getInstance().getSkinProvider().getSkin(profile));
+	}
+
+	public static PlayerSkin overrideSkin(GameProfile profile, PlayerSkin skin) {
+		MinecraftClient client = MinecraftClient.getInstance();
+
+		var skinFile = skinTextureFor(profile);
+		if (skinFile.isPresent()) {
+			// register skin texture
+			var texture = skinFile.get();
+			Identifier skinId = new Identifier("skin_overrides", "skin/" + profile.getId().toString());
+			client.getTextureManager().registerTexture(skinId, texture);
+			// update skin
+			skin = new PlayerSkin(skinId, null, skin.capeTexture(), skin.elytraTexture(), texture.model, false);
+		}
+
+		var userOverride = playerIdFor(profile);
+		if (userOverride.isPresent()) {
+			String name = userOverride.get();
+
+			// get the uuid
+			Optional<UUID> uuid = Optional.empty();
+			if (name.matches(UUID_REGEX)) {
+				// parse uuid
+				try {
+					uuid = Optional.of(name.contains("-") ? UUID.fromString(name) : UndashedUuid.fromString(name));
+				} catch (IllegalArgumentException e) {
+				}
+			} else {
+				// convert player username to uuid (cached)
+				var remoteProfile = client.getServer().getUserCache().findByName(name);
+				if (remoteProfile.isPresent()) {
+					uuid = Optional.of(remoteProfile.get().getId());
+				}
+			}
+
+			// if we have a uuid, get the full profile (cached)
+			if (uuid.isPresent()) {
+				var profileResult = client.getSessionService().fetchProfile(uuid.get(), false);
+
+				// set the skin to the remote one (cached)
+				if (profileResult != null) {
+					PlayerSkin remoteSkin = client.getSkinProvider().getSkinSupplier(profileResult.profile()).get();
+					skin = new PlayerSkin(remoteSkin.texture(), remoteSkin.textureUrl(), skin.capeTexture(),
+							skin.elytraTexture(), remoteSkin.model(), false);
+				}
+			}
+		}
+
+		var capeFile = capeTextureFor(profile);
+		if (capeFile.isPresent()) {
+			// register cape texture
+			Identifier capeId = new Identifier("skin_overrides", "cape/" + profile.getId().toString());
+			client.getTextureManager().registerTexture(capeId, capeFile.get());
+			// update skin
+			// note: the elytra texture is a separate part of the record,
+			// but updating the cape still updates the elytra.
+			skin = new PlayerSkin(skin.texture(), skin.textureUrl(), capeId, skin.elytraTexture(), skin.model(), false);
+		}
+
+		return skin;
+	}
 
 	public static Optional<LocalSkinTexture> skinTextureFor(GameProfile profile) {
 		return getTextureFor(SKIN_OVERRIDES, profile)
--- src/main/java/net/orifu/skin_overrides/mixin/PlayerListEntryMixin.java
+++ src/main/java/net/orifu/skin_overrides/mixin/PlayerListEntryMixin.java
@@ -1,7 +1,5 @@
 package net.orifu.skin_overrides.mixin;
 
-import java.util.Optional;
-import java.util.UUID;
 import java.util.function.Supplier;
 
 import org.jetbrains.annotations.Nullable;
@@ -12,12 +10,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.mojang.authlib.GameProfile;
-import com.mojang.util.UndashedUuid;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.texture.PlayerSkin;
-import net.minecraft.util.Identifier;
 import net.orifu.skin_overrides.SkinOverrides;
 
 @Mixin(PlayerListEntry.class)
@@ -33,71 +28,6 @@ public class PlayerListEntryMixin {
     @Inject(method = "getSkin", at = @At("RETURN"), cancellable = true)
     private void getSkin(CallbackInfoReturnable<PlayerSkin> cir) {
         PlayerSkin skin = cir.getReturnValue();
-        MinecraftClient client = MinecraftClient.getInstance();
-
-        var skinFile = SkinOverrides.skinTextureFor(this.profile);
-        if (skinFile.isPresent()) {
-            // register skin texture
-            var texture = skinFile.get();
-            Identifier skinId = new Identifier("skin_overrides", "skin/" + this.profile.getId().toString());
-            client.getTextureManager().registerTexture(skinId, texture);
-            // update skin
-            skin = new PlayerSkin(skinId, null, skin.capeTexture(), skin.elytraTexture(), texture.model, false);
-        }
-
-        var userOverride = SkinOverrides.playerIdFor(this.profile);
-        if (userOverride.isPresent()) {
-            String name = userOverride.get();
-
-            // new name, fetch the new profile
-            if (!name.equals(this.orifu$remoteName)) {
-                // get the uuid
-                Optional<UUID> uuid = Optional.empty();
-                if (name.matches(SkinOverrides.UUID_REGEX)) {
-                    // parse uuid
-                    try {
-                        uuid = Optional.of(name.contains("-") ? UUID.fromString(name) : UndashedUuid.fromString(name));
-                    } catch (IllegalArgumentException e) {
-                    }
-                } else {
-                    // convert player username to uuid
-                    var profile = client.getServer().getUserCache().findByName(name);
-                    if (profile.isPresent()) {
-                        uuid = Optional.of(profile.get().getId());
-                    }
-                }
-
-                // if we have a uuid, get the full profile
-                if (uuid.isPresent()) {
-                    var profileResult = client.getSessionService().fetchProfile(uuid.get(), false);
-
-                    // if this is a valid profile, use its skin
-                    if (profileResult != null) {
-                        this.orifu$remoteName = name;
-                        this.orifu$remoteSkin = client.getSkinProvider().getSkinSupplier(profileResult.profile());
-                    }
-                }
-            }
-
-            if (this.orifu$remoteSkin != null) {
-                // set the skin to the remote one
-                PlayerSkin remoteSkin = this.orifu$remoteSkin.get();
-                skin = new PlayerSkin(remoteSkin.texture(), remoteSkin.textureUrl(), skin.capeTexture(),
-                        skin.elytraTexture(), remoteSkin.model(), false);
-            }
-        }
-
-        var capeFile = SkinOverrides.capeTextureFor(this.profile);
-        if (capeFile.isPresent()) {
-            // register cape texture
-            Identifier capeId = new Identifier("skin_overrides", "cape/" + this.profile.getId().toString());
-            client.getTextureManager().registerTexture(capeId, capeFile.get());
-            // update skin
-            // note: the elytra texture is a separate part of the record,
-            // but updating the cape still updates the elytra.
-            skin = new PlayerSkin(skin.texture(), skin.textureUrl(), capeId, skin.elytraTexture(), skin.model(), false);
-        }
-
-        cir.setReturnValue(skin);
+        cir.setReturnValue(SkinOverrides.overrideSkin(this.profile, skin));
     }
 }
