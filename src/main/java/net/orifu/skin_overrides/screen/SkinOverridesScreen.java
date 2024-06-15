ab0e2652c32a8d24e09c461b4e6115e6c1a2b7bc
orifu <orifu@duck.com>
2024-06-15 20:45:25 +0100

    add tab and grid layout

--- src/main/java/net/orifu/skin_overrides/screen/SkinOverridesScreen.java
+++ src/main/java/net/orifu/skin_overrides/screen/SkinOverridesScreen.java
@@ -4,15 +4,29 @@ import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.world.CreateWorldScreen;
+import net.minecraft.client.gui.tab.GridWidgetTab;
+import net.minecraft.client.gui.tab.TabManager;
+import net.minecraft.client.gui.widget.HeaderBar;
 import net.minecraft.client.gui.widget.button.ButtonWidget;
+import net.minecraft.client.gui.widget.layout.FrameWidget;
+import net.minecraft.client.gui.widget.layout.GridWidget;
 import net.minecraft.text.CommonTexts;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.MathHelper;
 
 public class SkinOverridesScreen extends Screen {
     private static final Text TITLE = Text.translatable("skin_overrides.title");
+    private static final Text SKIN_TITLE = Text.translatable("skin_overrides.title.skin");
+    private static final Text CAPE_TITLE = Text.translatable("skin_overrides.title.cape");
+
+    private final TabManager tabManager = new TabManager(this::addDrawableSelectableElement, (wg) -> this.remove(wg));
 
     @Nullable
     private Screen parent;
+
+    private HeaderBar header;
+    private GridWidget grid;
 
     public SkinOverridesScreen(@Nullable Screen parent) {
         super(TITLE);
@@ -22,21 +36,56 @@ public class SkinOverridesScreen extends Screen {
 
     @Override
     protected void init() {
-        this.addDrawableSelectableElement(
-                ButtonWidget.builder(CommonTexts.DONE, (btn) -> this.closeScreen())
-                        .positionAndSize(this.width / 2 + 4, this.height - 48, 150, 20)
+        // add tabs header
+        this.header = this.addDrawableSelectableElement(
+                HeaderBar.builder(this.tabManager, this.width)
+                        .tabs(new OverridesTab(true), new OverridesTab(false))
                         .build());
+        this.header.setFocusedTab(0, false);
+
+        // add footer grid
+        this.grid = new GridWidget().setColumnSpacing(10);
+        var helper = this.grid.createAdditionHelper(2);
+
+        // done button
+        helper.add(ButtonWidget.builder(CommonTexts.DONE, (btn) -> this.closeScreen()).build());
+
+        this.grid.visitWidgets(this::addDrawableSelectableElement);
+
+        this.repositionElements();
     }
 
     @Override
     public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
         super.render(graphics, mouseX, mouseY, delta);
 
-        graphics.drawCenteredShadowedText(this.textRenderer, TITLE, this.width / 2, 8, 0xffffff);
+        graphics.drawTexture(CreateWorldScreen.FOOTER_SEPARATOR_TEXTURE, 0,
+                MathHelper.roundUpToMultiple(this.height - 36 - 2, 2), 0, 0, this.width, 2, 32, 2);
+    }
+
+    @Override
+    public void repositionElements() {
+        // reposition tab header
+        this.header.setWidth(this.width);
+        this.header.arrangeElements();
+
+        // reposition footer grid
+        this.grid.arrangeElements();
+        FrameWidget.align(this.grid, 0, this.height - 36, this.width, 36);
     }
 
     @Override
     public void closeScreen() {
         this.client.setScreen(this.parent);
+    }
+
+    class OverridesTab extends GridWidgetTab {
+        public final boolean isSkin;
+
+        public OverridesTab(boolean isSkin) {
+            super(isSkin ? SKIN_TITLE : CAPE_TITLE);
+
+            this.isSkin = isSkin;
+        }
     }
 }
