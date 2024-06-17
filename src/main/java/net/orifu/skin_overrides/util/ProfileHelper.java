0d9be22f1a413df414951d20171d0bc0d77d4c3d
orifu <orifu@duck.com>
2024-06-17 22:01:18 +0100

    case insensitive uuids

--- src/main/java/net/orifu/skin_overrides/util/ProfileHelper.java
+++ src/main/java/net/orifu/skin_overrides/util/ProfileHelper.java
@@ -12,7 +12,7 @@ import net.minecraft.util.UserCache;
 import net.minecraft.util.UuidUtil;
 
 public class ProfileHelper {
-    public static final String UUID_REGEX = "^[0-9a-f]{8}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{12}$";
+    public static final String UUID_REGEX = "^[0-9a-fA-F]{8}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{12}$";
 
     private static UserCache userCache;
 
