2a517fb33af24684a7cfbd8fb89e29e51b86802d
orifu <orifu@duck.com>
2024-07-07 16:10:43 +0100

    3D skin layers compatibility

--- src/main/java/net/orifu/skin_overrides/texture/LocalPlayerTexture.java
+++ src/main/java/net/orifu/skin_overrides/texture/LocalPlayerTexture.java
@@ -11,12 +11,13 @@ import net.minecraft.resource.ResourceManager;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Objects;
 
 public class LocalPlayerTexture extends PlayerSkinTexture {
     private final File textureFile;
 
     public LocalPlayerTexture(File textureFile, boolean isSkin) {
-        super(null, null, null, isSkin, null);
+        super(textureFile, null, null, isSkin, null);
 
         this.textureFile = textureFile;
     }
@@ -30,8 +31,8 @@ public class LocalPlayerTexture extends PlayerSkinTexture {
         MinecraftClient.getInstance().execute(() -> {
             try {
                 NativeImage texture = this.loadTexture(new FileInputStream(this.textureFile));
-                this.onTextureLoaded(texture);
-            } catch (FileNotFoundException e) {
+                this.onTextureLoaded(Objects.requireNonNull(texture));
+            } catch (FileNotFoundException | NullPointerException e) {
                 // shouldn't happen
             }
         });
