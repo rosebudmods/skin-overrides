76037c04486164e70b904a40f66da4857953dd2f
orifu <orifu@duck.com>
2024-07-13 00:31:10 +0100

    1.14.4 port

--- src/main/java/net/orifu/skin_overrides/texture/LocalPlayerTexture.java
+++ src/main/java/net/orifu/skin_overrides/texture/LocalPlayerTexture.java
@@ -10,14 +10,20 @@ import net.minecraft.resource.ResourceManager;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Objects;
 
 public class LocalPlayerTexture extends PlayerSkinTexture {
     private final File textureFile;
 
     public LocalPlayerTexture(File textureFile, boolean isSkin) {
-        super(textureFile, null, null, isSkin, null);
+        super(
+                textureFile,
+                null,
+                null,
+                /*? if >=1.15.2 {*/ isSkin, null
+                /*?} else*/ /*isSkin ? new net.minecraft.client.texture.SkinRemappingImageFilter() : null*/
+        );
 
         this.textureFile = textureFile;
     }
@@ -30,9 +36,17 @@ public class LocalPlayerTexture extends PlayerSkinTexture {
     public void load(ResourceManager manager) {
         MinecraftClient.getInstance().execute(() -> {
             try {
+                //? if >=1.15.2 {
                 NativeImage texture = this.loadTexture(new FileInputStream(this.textureFile));
                 this.onTextureLoaded(Objects.requireNonNull(texture));
-            } catch (FileNotFoundException | NullPointerException e) {
+                //?} else {
+                /*NativeImage texture = NativeImage.read(new FileInputStream(this.textureFile));
+                if (this.filter != null) {
+                    texture = this.filter.filterImage(texture);
+                }
+                this.method_4534(texture);
+                *///?}
+            } catch (NullPointerException | IOException e) {
                 // shouldn't happen
             }
         });
