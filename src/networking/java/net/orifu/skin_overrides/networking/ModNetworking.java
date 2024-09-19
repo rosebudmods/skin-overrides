ce0dd5e94eecd88a64bd7166e2e1e65e5b1283f1
orifu <orifu@duck.com>
2024-09-19 05:26:10 +0100

    the packet works!

--- src/networking/java/net/orifu/skin_overrides/networking/ModNetworking.java
+++ src/networking/java/net/orifu/skin_overrides/networking/ModNetworking.java
@@ -3,6 +3,12 @@ package net.orifu.skin_overrides.networking;
 import com.mojang.authlib.properties.Property;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.network.packet.s2c.PlayerRemovalS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+import java.util.Collections;
 
 public class ModNetworking {
     public static void init() {
@@ -15,10 +21,21 @@ public class ModNetworking {
             System.out.println(" - value: " + payload.skinValue());
             System.out.println(" - signt: " + payload.signature());
 
-            var profile = ctx.player().getGameProfile();
-            profile.getProperties().put(
+            ServerPlayerEntity player = ctx.player();
+            PlayerManager playerManager = ctx.server().getPlayerManager();
+
+            // set skin
+            player.getGameProfile().getProperties().put(
                     "textures",
                     new Property("textures", payload.skinValue(), payload.signature()));
+
+            // remove and re-add player (updates skin in tab list)
+            playerManager.sendToAll(new PlayerRemovalS2CPacket(Collections.singletonList(ctx.player().getUuid())));
+            playerManager.sendToAll(PlayerListS2CPacket.create(Collections.singleton(player)));
+
+            // update skin for players that are tracking this player
+            var tracker = player.getServerWorld().getChunkManager().delegate.entityTrackers.get(player.getId());
+            tracker.listeners.forEach(listener -> tracker.entry.startTracking(listener.getPlayer()));
         });
     }
 }
